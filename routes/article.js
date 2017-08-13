db724e0e407e1ded7be51eb41c801e2fb5e3848f
Diego Iastrubni <diegoiast@gmail.com>
2017-08-14 01:55:48 +0300

    Displaying of single article mostly works
    - direction of page is RTL
    - we convert cp1255 to utf8
    - images served from whatsup have 'src' modified
    - we can parser different meta data of each article

--- routes/article.js
+++ routes/article.js
@@ -10,39 +10,45 @@ router.get('/:id', function(req, res, next) {
     var url = "https://whatsup.org.il/modules.php?op=modload&name=News&file=article&sid=" + articleID;
     console.log('GET: %s', url);
 
-    request(url, function(err, body) {
-        console.log(err || body.body); // Print out the HTML
+    var options = {
+        method: 'GET',
+        url: url,
+        encoding: 'binary' 
+    };
 
-        /*
-        <div class="ng_article">
-        <h1 class="ng_article_title">
-            <a title="Short URL" href="/article/7280">
-                <img style="border:0; vertical-align:bottom;" alt="quick_link" src="/images/whatsup/cat_folder_big.gif">
-            </a>
-            <a class="pn-title" href="modules.php?op=modload&amp;name=News&amp;file=article&amp;sid=7280&amp;mode=nested&amp;order=0&amp;thold=0">
-            נפתח ה-CFP לאוגוסט פינגווין הקרוב
-            </a>
-        </h1>
-
-        <span class="pn-art">
-            <a class="pn-normal" href="modules.php?op=modload&amp;name=News&amp;file=index&amp;catid=&amp;topic=20">
-                <img src="images/topics/announce.png" border="0" Alt="הכרזות" align="right" hspace="5" vspace="5" >
-            </a>
-            ה-CFP, קול קורא להרצאות, לקראת <a href="http://ap.hamakor.org.il/2017/">אוגוסט פינגווין הקרוב</a> פתוח, אתם מוזמנים להגיש את הרצאותיכם לכנס שיתקיים ב-7-8 לספטמבר במכללת שנקר ברמת גן.<br><br>
-            השנה הכנס ייערך במתכונת של יומיים ומגוון מסלולים, אם ברצונכם להרצות בהם, השתמשו ב<a href="https://docs.google.com/forms/d/e/1FAIpQLSc4x2XDQ36LoqeQMzk6p9qrZ25xFtOC9YWIE3KZQ0faGfxxjg/viewform">טופס להגשת הצעה להרצאה</a>.<br><br>
-            תודה,<br>
-            משה, בשם צוות המארגנים<BR><BR>
-        </span>
-        */
-
-        let utf8Body = body.body; // encoding.convert(body.body, "CP1255", 'UTF8').toString();
+    request(options, function(err, body) {
+        console.log(err); // Print out the HTML
+        let utf8Body = encoding.convert(body.body, 'UTF8', 'CP1255').toString();
         let $ = cheerio.load(utf8Body);
-        let articleTitle = $("div.ng_article h1.ng_article_title a.pn-title").text()
-        let articleContent = $("span.pn-art").text()
+
+        let articleHTML = $("div.ng_article");
+        let articleTitle = articleHTML.find("h1.ng_article_title a.pn-title").text()
+        let articleMetaData = articleHTML.find("div.ng_info_row").text().split("·")
+        let articleDate = articleMetaData[0]
+        let articleAuthor = articleMetaData[1]
+        let articleTopic = articleMetaData[2]
+        var articleContent = articleHTML.find("span.pn-art").remove("a.pn-normal")
+
+        articleContent.find("img").each(function(i, element){
+            var origSrc = $(element).attr("src");
+            if (origSrc.startsWith("/")) {
+                $(element).attr("src", "https://whatsup.org.il" + origSrc);                
+            }
+        })
+
+        articleContent.find("a").each(function(i, element){
+            var origHref = $(element).attr("href");
+            // console.log("a %d", origHref);
+            if (origHref.startsWith("/")) {
+                $(element).attr("href", "https://whatsup.org.il" + origHref);
+            }
+        })
 
         res.render('article', {
             title: articleTitle,
-            content: articleContent
+            date: articleDate,
+            author: articleAuthor,
+            content: articleContent.html()
         });
     });
 });
