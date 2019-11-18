86103cd590cd51d00ea817c1002f92d6ad33e21c
Diego Iastrubni <diegoiast@gmail.com>
2019-11-18 18:34:14 +0200

    Fix favico thingie

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ var topic = require('./routes/topic');
 var api = require('./routes/api')
 var app = express();
 
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
